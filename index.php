9b064ba06037634527711784c20e7c0bd46b8a44
Sergey <31823239+SeAnSolovev@users.noreply.github.com>
2023-03-05 14:39:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,29 @@
 <?php
-	header('Content-Type: text/html; charset=utf-8');
-	header('X-Powered-By: EGP');
+    header('Content-Type: text/html; charset=utf-8');
+    header('X-Powered-By: EGP');
 
-	date_default_timezone_set('Europe/Moscow');
+    date_default_timezone_set('Europe/Moscow');
 
-	@ini_set('display_errors', TRUE);
-	@ini_set('html_errors', TRUE);
-	@ini_set('error_reporting', E_ALL ^ E_WARNING ^ E_NOTICE ^ E_STRICT);
+    @ini_set('display_errors', TRUE);
+    @ini_set('html_errors', TRUE);
+    @ini_set('error_reporting', E_ALL ^ E_WARNING ^ E_NOTICE ^ E_STRICT);
 
-	DEFINE('EGP', TRUE);
-	DEFINE('DIR', dirname('index.php'));
-	DEFINE('ROOT', DIR.'/');
-	DEFINE('SYS', ROOT.'system/');
-	DEFINE('TPL', ROOT.'template/');
-	DEFINE('TEMP', ROOT.'temp/');
-	DEFINE('FILES', ROOT.'files/');
-	DEFINE('DATA', SYS.'data/');
-	DEFINE('LIB', SYS.'library/');
-	DEFINE('ENG', SYS.'engine/');
-	DEFINE('SEC', SYS.'sections/');
+    DEFINE('EGP', TRUE);
+    DEFINE('DIR', dirname('index.php'));
+    DEFINE('ROOT', DIR.'/');
+    DEFINE('SYS', ROOT.'system/');
+    DEFINE('TPL', ROOT.'template/');
+    DEFINE('TEMP', ROOT.'temp/');
+    DEFINE('FILES', ROOT.'files/');
+    DEFINE('DATA', SYS.'data/');
+    DEFINE('LIB', SYS.'library/');
+    DEFINE('ENG', SYS.'engine/');
+    DEFINE('SEC', SYS.'sections/');
 
-	$start_point = $_SERVER['REQUEST_TIME'];
+    $start_point = $_SERVER['REQUEST_TIME'];
 
-	$mcache = new Memcache;
-	$mcache->connect('127.0.0.1', 11211) or exit('Ошибка подключения Memcache');
+    $mcache = new Memcache;
+    $mcache->connect('127.0.0.1', 11211) or exit('Ошибка подключения Memcache');
 
     // Composer
     if (!file_exists(ROOT.'vendor/autoload.php')) {
@@ -31,22 +31,22 @@
     }
     require(ROOT.'vendor/autoload.php');
 
-	// Настройки
-	include(DATA.'config.php');
-	include(DATA.'engine.php');
-	include(DATA.'mysql.php');
-	include(DATA.'params.php');
+    // Настройки
+    include(DATA.'config.php');
+    include(DATA.'engine.php');
+    include(DATA.'mysql.php');
+    include(DATA.'params.php');
 
-	// Библиотеки
-	include(LIB.'sql.php');
-	include(LIB.'html.php');
-	include(LIB.'system.php');
+    // Библиотеки
+    include(LIB.'sql.php');
+    include(LIB.'html.php');
+    include(LIB.'system.php');
 
-	$uip = sys::ip();
+    $uip = sys::ip();
 
-	// Распределитель
-	include(SYS.'distributor.php');
+    // Распределитель
+    include(SYS.'distributor.php');
 
-	// Выхлоп
-	echo $html->arr['all'];
+    // Выхлоп
+    echo $html->arr['all'];
 ?>
