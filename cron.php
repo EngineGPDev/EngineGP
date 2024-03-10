e7d275347402c58046769ac4d53fc4a43e752fb8
Sergei Solovev <ssseee580@gmail.com>
2024-03-10 23:03:58 +0300

    Added cron error output to the file

--- cron.php
+++ cron.php
@@ -23,6 +23,12 @@ $start_point = $_SERVER['REQUEST_TIME'];
 $mcache = new Memcache;
 $mcache->connect('127.0.0.1', 11211) or exit('Ошибка подключения Memcache.' . PHP_EOL);
 
+// Composer
+if (!file_exists(ROOT . 'vendor/autoload.php')) {
+    die('Please <a href="https://getcomposer.org/download/" target="_blank" rel="noreferrer" style="color:#0a25bb;">install composer</a> and run <code style="background:#222;color:#00e01f;padding:2px 6px;border-radius:3px;">composer install</code>');
+}
+require(ROOT . 'vendor/autoload.php');
+
 // Настройки
 include(DATA . 'config.php');
 
