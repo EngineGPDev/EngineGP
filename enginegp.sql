fcca4e4d721b398c80daf246392c35255c976063
Sergei Solovev <ssseee580@gmail.com>
2024-04-10 23:22:40 +0300

    Removed copyright in enginegp.sql

--- enginegp.sql
+++ enginegp.sql
@@ -2188,16 +2188,6 @@ ALTER TABLE `users`
 --
 ALTER TABLE `web`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-/*
- * EngineGP   (https://enginegp.ru or https://enginegp.com)
- *
- * @link      https://github.com/EngineGPDev/EngineGP
- * @link      https://gitforge.ru/EngineGP/EngineGP
- * @copyright Copyright (c) Solovev Sergei <inbox@seansolovev.ru>
- * @license   https://github.com/EngineGPDev/EngineGP/blob/main/LICENSE
- * @license   https://gitforge.ru/EngineGP/EngineGP/src/branch/main/LICENSE
- */
-
 --
 -- AUTO_INCREMENT для таблицы `wiki`
 --
