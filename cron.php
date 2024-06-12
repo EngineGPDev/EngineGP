09ae9f6329a99d1ff13a6c353a01aee3960c8ae2
Sergei Solovev <ssseee580@gmail.com>
2024-06-12 13:28:08 +0300

    Update copyright

--- cron.php
+++ cron.php
@@ -5,10 +5,8 @@
  * @copyright Copyright (c) 2018-present Solovev Sergei <inbox@seansolovev.ru>
  *
  * @link      https://github.com/EngineGPDev/EngineGP for the canonical source repository
- * @link      https://gitforge.ru/EngineGP/EngineGP for the canonical source repository
  *
  * @license   https://github.com/EngineGPDev/EngineGP/blob/main/LICENSE MIT License
- * @license   https://gitforge.ru/EngineGP/EngineGP/src/branch/main/LICENSE MIT License
  */
 
 date_default_timezone_set('Europe/Moscow');
